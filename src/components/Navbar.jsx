4edd23a97fefeb57a4eac61b8d441653c247d1a5
Nancy Emad <nancyemadd0099@gmail.com>
2026-01-13 00:48:25 +0200

    update navbar to scrolleffect

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,29 @@
 
+
+import { useEffect, useState } from 'react';
 import './Navbar.css'
 
 function Navbar() {
+
+    const [scrolled, setScrolled] = useState(false)
+
+    useEffect(() => {
+        const handelScroll = () => {
+            const scrollLimit = window.innerHeight * 0.1;
+
+            if (window.scrollY > scrollLimit)  {
+                setScrolled(true);
+            } else {
+                setScrolled(false)
+            }
+        }
+        window.addEventListener("scroll", handelScroll);
+        return () => window.removeEventListener("scroll", handelScroll)
+    }, [])
+
     return (
         <>
-            <nav>
+            <nav className={scrolled ? "scrolled" : ""}>
                 <div className="container">
                     <div className="logo">
                         <p>N</p>
@@ -18,7 +37,7 @@ function Navbar() {
                         <a href="#">Contact</a>
                     </div>
                 </div>
-        </nav>
+            </nav>
         </>
     )
 }
