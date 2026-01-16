ad48a2230ede371440bdc1150eb7735136c0eb30
Nancy Emad <nancyemadd0099@gmail.com>
2026-01-16 17:32:48 +0200

    add responsive to contact page and footer

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,8 @@ function App() {
       <Projects />
       <LetsProject />
       <Skills />
-      {/* <Contact /> */}
-      {/* <Footer /> */}
+      <Contact />
+      <Footer />
     </>
   )
 }
